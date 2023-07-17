c9cef62000bdfbe6454a348e7d878d452966f82b
Vitaly Turovsky <vital2580@icloud.com>
2023-07-17 16:49:35 +0300

    0.0.1

--- build-electron.mjs
+++ build-electron.mjs
@@ -1,7 +1,16 @@
 import { main } from '@zardoy/electron-esbuild'
 
+const version = process.env.npm_package_version
+console.log(version)
+
 await main({
     mode: 'production',
     outdir: 'build',
+    esbuildOptions: {
+        define: {
+            'process.env.NODE_ENV': '"production"',
+            'process.env.VERSION': `"${version}"`,
+        },
+    },
 })
 // TODO download server file!
